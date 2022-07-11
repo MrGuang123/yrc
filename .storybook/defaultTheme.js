1fc9e7fc0d14b3885045f5541e384ef6091ea7c9
ytg <522125842@qq.com>
2022-07-11 21:58:39 +0800

    chore: add css theme 81%

--- .storybook/defaultTheme.js
+++ .storybook/defaultTheme.js
@@ -1,13 +1,35 @@
 import { create } from '@storybook/theming'
 
+// https://storybook.js.org/docs/react/configure/theming#global-theming
 export default create({
   base: 'dark',
-  brandTitle: 'MrGuang 组件库',
+
+  brandTitle: 'YRC 组件库',
+  brandUrl: '',
+  brandImage: './wukong.png',
+  brandTarget: '_self',
+
+  colorPrimary: 'hotpink',
+  colorSecondary: 'deepskyblue',
+
+  // 界面
   appBg: '#1b1b24',
   appContentBg: '#1b1b24',
+  appBorderColor: 'grey',
   appBorderRadius: 4,
+
+  // 排版
+  textColor: 'white',
+  textInverseColor: 'rgba(0, 0, 0, .9)',
+
+  // 工具栏样式
   barTextColor: 'white',
+  barSelectedColor: 'black',
   barBg: '#1b1b24',
-  appBorderRadius: 4,
-  inputBorderRadius: 4
+
+  // 表单颜色
+  inputBg: 'white',
+  inputBorder: 'silver',
+  inputTextColor: 'black',
+  inputBorderRadius: 4,
 })
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import { themes } from '@storybook/theming'
+import ThemeWrapper from '../src/components/ThemeWrapper'
 // import React from 'react';
 // import ThemeWrapper from '../src/components/ThemeWrapper'
 // import { CssReset } from '../src/components/Theme'
@@ -11,7 +13,12 @@
 
 // export const decorators = [withThemeProvider];
 
+// ThemeProvider相当于提供了一个theme的context和theme的缓存
+
 export const parameters = {
+  docs: {
+    theme: themes.dark
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
